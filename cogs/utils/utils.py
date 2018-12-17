8dc5c81d8e56589ee2dc8da8289d20bd6299e55a
Makupi <makupi@icloud.com>
2018-12-17 22:55:27 +0100

    Bugfix uptime, Adds servertime and serverinfo

--- cogs/utils/utils.py
+++ cogs/utils/utils.py
@@ -29,13 +29,36 @@ class Utils:
     @commands.command()
     async def uptime(self, ctx):
         ''': See how long I've been online'''
-        time = datetime.now() - self.bot.starttime
-        days = time.days
-        hours, remainder = divmod(time.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        await ctx.channel.send(f"I've been online for {days} days, {minutes} min, {seconds} seconds!")
+        _uptime = datetime.now() - self.bot.starttime
+        await ctx.channel.send(f'I\'ve been online for `{_uptime}`.')
 
     @commands.command()
     async def starttime(self, ctx):
         ''': See when I went online'''
         await ctx.channel.send(f'Running since: `{self.bot.starttime}`.')
+
+    @commands.command()
+    async def servertime(self, ctx):
+        ''': See what my current time is'''
+        await ctx.channel.send(f'Current Servertime: `{datetime.now()}`.')
+    
+    @commands.command()
+    async def serverinfo(self, ctx):
+        ''': Show info to current server'''
+        region = ctx.guild.region
+        name = ctx.guild.name
+        icon_url = ctx.guild.icon_url
+        owner = ctx.guild.owner
+        text_channels = ctx.guild.text_channels
+        voice_channels = ctx.guild.voice_channels
+        members = ctx.guild.members
+
+        embed = discord.Embed(title=name, description=f'Server Information', color=discord.Color(0xff8eed))
+        embed.set_thumbnail(url=icon_url)
+        embed.set_footer(text=f'Server ID <{ctx.guild.id}>')
+        embed.add_field(name='Owner', value=f'{owner}')
+        embed.add_field(name='Users', value=f'{len(members)} ({len([member for member in members if member.status is not discord.Status.offline])} online)')
+        embed.add_field(name='Text Channels', value=f'{len(text_channels)}')
+        embed.add_field(name='Voice Channels', value=f'{len(voice_channels)}')
+        embed.add_field(name='Region', value=f'{region}')
+        await ctx.channel.send(embed=embed)
