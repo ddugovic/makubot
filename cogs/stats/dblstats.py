f7cd34a799e80ebcf68b4de6728ecf87629ecd85
makubob <makupi@icloud.com>
2020-04-23 19:17:51 +0200

    Changes DBLStats to use dblpy library

--- cogs/stats/dblstats.py
+++ cogs/stats/dblstats.py
@@ -2,37 +2,42 @@ import aiohttp
 import sys, traceback
 from utils import Config
 from discord.ext import commands
+import dbl
 
 
 class DBLStats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    async def post_count(self):
-        if not self.token:  # can never happen theoretically
-            return
-
-        payload = {"server_count": len(self.bot.guilds)}
-        async with aiohttp.ClientSession() as aioclient:
-            await aioclient.post(self.url, data=payload, headers=self.headers)
-
-    async def on_ready(self):
         config = Config.get_config()
         try:
             self.token = config["dblapi"]
-            self.headers = {"Authorization": self.token}
-            self.url = f"https://discordbots.org/api/bots/{str(self.bot.user.id)}/stats"
         except Exception as ex:
             print(f"exception - unloading extension dblstats: {ex}")
             traceback.print_exc(file=sys.stdout)
             self.bot.unload_extension("cogs.utils.dblstats")
-        await self.post_count()
+        self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True)
 
-    async def on_guild_join(self, guild):
-        await self.post_count()
+    # async def post_count(self):
+    #     if not self.token:  # can never happen theoretically
+    #         return
 
-    async def on_guild_remove(self, guild):
-        await self.post_count()
+    #     payload = {"server_count": len(self.bot.guilds)}
+    #     async with aiohttp.ClientSession() as aioclient:
+    #         await aioclient.post(self.url, data=payload, headers=self.headers)
+
+    async def on_ready(self):
+        print('DBLStats ready.')
+        #     self.headers = {"Authorization": self.token}
+        #     self.url = f"https://discordbots.org/api/bots/{str(self.bot.user.id)}/stats"
+        # except Exception as ex:
+            
+        # await self.post_count()
+
+    # async def on_guild_join(self, guild):
+    #     await self.post_count()
+
+    # async def on_guild_remove(self, guild):
+    #     await self.post_count()
 
 
 def setup(bot):
